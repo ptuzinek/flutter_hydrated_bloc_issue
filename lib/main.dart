ceb9be7a66698037553021178622301344b8bdff
Felix Angelov <felangelov@gmail.com>
2021-11-23 17:21:15 -0600

    fix: use flutter_services_binding

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hydrated_bloc_issue/cubit/example/example_cubit.dart';
 import 'package:flutter_hydrated_bloc_issue/screens/home_screen.dart';
+import 'package:flutter_services_binding/flutter_services_binding.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  FlutterServicesBinding.ensureInitialized();
 
   final storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
